cbb6efae47a5253adbcd4f6c7f2f7aa782fb0b89
Igor Chernyshov <ichernyshov@bk.ru>
2023-09-03 09:53:26 +0300

    Initial Commit

---
+++ brandsInDisguise/brandsInDisguiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  brandsInDisguiseApp.swift
+//  brandsInDisguise
+//
+//  Created by Игорь Чернышов on 03.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct brandsInDisguiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
