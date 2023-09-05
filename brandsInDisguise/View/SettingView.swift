f2d22c7bf65308d644768a2e8524f583e5cc099b
Igor Chernyshov <ichernyshov@bk.ru>
2023-09-05 21:17:39 +0300

    Added settings view

---
+++ brandsInDisguise/View/SettingView.swift
@@ -0,0 +1,38 @@
+//
+//  SettingView.swift
+//  brandsInDisguise
+//
+//  Created by Игорь Чернышов on 04.09.2023.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+	let imageSystemName: String
+	let buttonText: String
+	let action: () -> Void
+
+	var body: some View {
+		Button {
+			action()
+		} label: {
+			HStack {
+				Image(systemName: imageSystemName)
+					.foregroundStyle(.cyan)
+
+				Text(buttonText)
+					.tint(.primary)
+			}
+			.frame(width: 300, height: 48)
+			.overlay(
+				RoundedRectangle(cornerRadius: 25)
+					.stroke(.white, lineWidth: 2)
+			)
+		}
+	}
+}
+
+#Preview {
+	SettingView(imageSystemName: "heart.fill", buttonText: "Оценить приложение", action: { print("Did tap") })
+		.preferredColorScheme(.dark)
+}
--- brandsInDisguise/View/SettingsView.swift
+++ brandsInDisguise/View/SettingsView.swift
@@ -5,14 +5,42 @@
 //  Created by Игорь Чернышов on 04.09.2023.
 //
 
+import StoreKit
 import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		VStack(spacing: 24) {
+			SettingView(imageSystemName: "envelope.fill",
+						buttonText: "Отправить предложение",
+						action: { sendSuggestion() })
+
+			SettingView(imageSystemName: "heart.fill",
+						buttonText: "Оценить приложение",
+						action: { requestReview() })
+
+			SettingView(imageSystemName: "hand.thumbsup.fill",
+						buttonText: "Поддержать разработчиков",
+						action: {  })
+		}
     }
+
+	private func sendSuggestion() {
+		if let url = URL(string: "mailto:ichernyshov@bk.ru") {
+			UIApplication.shared.open(url)
+		}
+	}
+
+	private func requestReview() {
+		guard let currentScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+			print("Unable to get current scene")
+			return
+		}
+		SKStoreReviewController.requestReview(in: currentScene)
+	}
 }
 
 #Preview {
     SettingsView()
+		.preferredColorScheme(.dark)
 }
